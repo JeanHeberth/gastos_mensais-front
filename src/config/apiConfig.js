b2a294fb43d18637e5edef69b064218ca26aacff
Jean Heberth Souza Vieira <jeanheberth19@gmail.com>
2026-05-20 12:41:36 -0300

    alterando caminho da apiURL

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,1 +1,3 @@
-export const API_URL = "http://localhost:9998/gastos-mensais/";
+export const
+    API_URL = "http://100.83.72.100:9999/gastos-mensais/";
+    // API_URL = "http://localhost:9998/gastos-mensais/";
